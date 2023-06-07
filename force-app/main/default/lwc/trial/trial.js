bb2467caa43b3e533d57136666e87a3ceab48f5b
Cory Rhodes <rhodes.cory@gmail.com>
2023-06-06 23:46:50 -0500

    hey new branch change

--- force-app/main/default/lwc/trial/trial.js
+++ force-app/main/default/lwc/trial/trial.js
@@ -18,6 +18,8 @@ export default class Trial extends LightningElement {
         @track selection;
         @track selectionTwo;
 
+        // forcefield
+
         // CREATE MONTHLY CALENDAR
         bigRecords = [
             {month:'January', numberOfDays:['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']},
